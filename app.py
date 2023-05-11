bce001dbd1056f824c3f8538e6af2caa84d353a9
AR942 <63624017+AR942@users.noreply.github.com>
2023-05-11 14:25:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -186,4 +186,24 @@ print("Precision : ", precision_score(y_test, y_pred))
 print("Recall : ", recall_score(y_test, y_pred))
 print("F1 Score : ", f1_score(y_test, y_pred))
 
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Embedding, LSTM, Dropout
+from tensorflow.keras.callbacks import EarlyStopping
+tokenizer = Tokenizer(num_words=5000, lower=True)
+tokenizer.fit_on_texts(X_train_text)
+X_train_text_seq = tokenizer.texts_to_sequences(X_train_text)
+X_test_text_seq = tokenizer.texts_to_sequences(X_test_text)
+X_train_text_pad = pad_sequences(X_train_text_seq, maxlen=100)
+X_test_text_pad = pad_sequences(X_test_text_seq, maxlen=100)
+model = Sequential()
+model.add(Embedding(5000, 32, input_length=100))
+model.add(LSTM(64, dropout=0.2, recurrent_dropout=0.2))
+model.add(Dense(1, activation='sigmoid'))
+model.add(Dense(1, activation='sigmoid'))
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+early_stop = EarlyStopping(monitor='val_loss', patience=3)
+model.fit([X_train_text_pad, X_train_mwc_norm], y_train, epochs=10, batch_size=32, validation_split=0.2, callbacks=[early_stop])
 
+model.evaluate([X_test_text_pad, X_test_mwc_norm], y_test)
